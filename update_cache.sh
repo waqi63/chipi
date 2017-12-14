4d79d1dc1a08691d8d4886d7953c12512650ff76
waqi63 <34557322+waqi63@users.noreply.github.com>
2017-12-15 07:34:09 +0800

    Update update_cache.sh

--- update_cache.sh
+++ update_cache.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#
 
+sudo nvram -c
+sudo purge
+sudo rm -f /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache && sudo touch /System/Library/Extensions && sudo kextcache -u /
+sudo update_dyld_shared_cache -force
